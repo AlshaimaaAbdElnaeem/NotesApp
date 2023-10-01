a7b333269e4720b9c9455fbe958ffe8d1aa68769
AlshaimaaAbdElnaeem <alshaimaaabdelnaeem@gmail.com>
2023-10-01 09:03:52 +0300

    add add note state in bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubit/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/widgets/add_bottom.dart';
 import 'package:note_app/widgets/add_new_note.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -6,10 +10,24 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return  SingleChildScrollView(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        child: AddNewNote(),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+           if (state is AddNoteSuccess){
+          Navigator.pop(context);
+           }
+           else if (state is AddNoteFailur){
+print ('failed ${state.errorMessage}');
+           }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoadin ? true : false ,
+              child:const AddNewNote());
+          },
+        ),
       ),
     );
   }
