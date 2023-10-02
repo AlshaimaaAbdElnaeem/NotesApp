07451bcc633e5c8db3aa55087b0ae2bc7487286a
AlshaimaaAbdElnaeem <alshaimaaabdelnaeem@gmail.com>
2023-10-02 08:24:11 +0300

    create get notes method

--- lib/cubit/notes_cubit/notes_cubit.dart
+++ lib/cubit/notes_cubit/notes_cubit.dart
@@ -1,8 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:note_app/models/note_model.dart';
+import 'package:note_app/widgets/constance.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  getAllNotes (){
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+     List <NoteModel>notes =  noteBox.values.toList();
+      emit(NotesSucsses(notes: notes));
+    } catch (e) {
+      emit(NotesFailure(errorMessage: e.toString()));
+    }
+  }
 }
